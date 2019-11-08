090447d9342f01f11b569aeed9d657f6b315d197
Wooyong <whdrmt12@gmail.com>
2019-11-08 07:32:40 +0000

    revise __init__

--- modules/__init__.py
+++ modules/__init__.py
@@ -2,5 +2,5 @@ import modules.doc_graph
 import modules.scouter
 import modules.multi_summ
 import modules.timeline_summ
-import modules.image_ret
+import modules.image_selection
 import modules.utils
