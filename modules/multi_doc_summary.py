9d5828cbdc8e9ae501a7034d0e2599c8a2583778
Wooyong <whdrmt12@gmail.com>
2019-11-08 07:10:21 +0000

    recover multi_summ

--- modules/multi_doc_summary.py
+++ modules/multi_doc_summary.py
@@ -1,9 +1,9 @@
 import os
 
 import multiprocessing as mp
+from modules.multi_summ.multi_doc_sum import mds
 from modules.base_module import BaseModule
 
-from modules.multi_summ.main import stc_extract, make_summary
 
 
 class MultiDocSummary(BaseModule):
@@ -12,28 +12,23 @@ class MultiDocSummary(BaseModule):
     Args:
         topic (string): a keyword query
     '''
-    def __init__(self, topic, out_path):
+    def __init__(self, gpu):
+        '''
+        Args:
+            gpu (str or int): gpu number
+        '''
         super(MultiDocSummary, self).__init__(topic, out_path)
-
-    def process_data(self, top_doc_morph_sentence_list, top_keyword_list):
+        self.mds = mds(gpu)
+        
+    def process_data(self, documents_list):
         '''
         Overrided method from BaseModule class
         Generate a summary and convert results composed of morphemes to sentences with words
         Args:
-            top_doc_morph_sentence_list (list): a list of morph sentences in top documents
-            top_keyword_list (list): a list of keywords of top documents
+            documents_list (list): a list of plain documents [str, str, ..., ]
         '''
-        data_list = stc_extract(self.topic, top_keyword_list, top_doc_morph_sentence_list) #파라메터 추가
         
-        print("paraent pid", os.getpid())
-        ctx = mp.get_context('spawn')
-        queue = ctx.Queue()
-        p = ctx.Process(target=make_summary, args=(queue, self.topic, data_list))
-        p.start()
-        
-        self.main_json, self.detail_json = queue.get()
-    
-        #p.join(3) # timeout 안 설정하면 안끝남
+        return self.mds.m_translate(documents_list)
         
     def generate_json(self):
         '''
@@ -42,3 +37,7 @@ class MultiDocSummary(BaseModule):
         Args:
         '''
         return self.main_json, self.detail_json
+
+if __name__ == '__main__':
+    example = ['aa', 'bb', 'cc']
+    print(multi_doc_sum.m_translate(example))
