ffa79e4be877cd80783d9ccdaf0a8677ab771d32
Junmo-Park <knight2683@gmail.com>
2019-11-12 06:57:58 +0000

    fix outlook

--- modules/outlook.py
+++ modules/outlook.py
@@ -1,4 +1,4 @@
-from modules.outlook.get_forecast_sen import forecasting
+from modules.forecast.get_forecast_sen import forecasting
 from modules.base_module import BaseModule
 
 class ForecastSentence(BaseModule):
@@ -7,18 +7,22 @@ class ForecastSentence(BaseModule):
     Args:
         doc_list(list): article list to extract forecast sentences
     '''
-    def __init__(self, gpu):
+    def __init__(self, topic, out_path):
         '''
         :param gpu: gpu number
         '''
-        super(ForecastSentence, self).__init__(topic, out_path)
-        self.forecasting = forecasting(gpu)
+        self.topic = topic
+        self.out_path = out_path
+        self.gpu = 0
+        self.forecasting = forecasting()
+        # super(ForecastSentence, self).__init__(topic, out_path)
+        # self.forecasting = forecasting(gpu)
 
-    def process_data(self, doc_list):
+    def process_data(self, doc_list, today):
         '''
         Overrided method from BaseModule class
         Args:
             documents_list (list): a list of plain documents [str, str, ..., ]
         '''
 
-        return self.forecasting.forecast_sentences(doc_list)
+        return self.forecasting.forecast_sentences(doc_list, today)
