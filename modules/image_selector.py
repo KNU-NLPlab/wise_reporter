2006dbad586ec0a043628b7ca102d211e38b229b
ChanghunHyun <52402295+ChanghunHyun@users.noreply.github.com>
2019-11-28 18:01:33 +0900

    Update image_selector.py

--- modules/image_selector.py
+++ modules/image_selector.py
@@ -3,12 +3,10 @@ from modules.image_selection.image_selection_2019_v2 import *
 from keras.models import load_model
 from elasticsearch import Elasticsearch
 
-import re
-
 class ImageSelectionModule(BaseModule):
     def __init__(self, out_path):        
         self.out_path = out_path
-        self.vggModel = load_model('modules/image_selection/weight_best.hdf') ###
+        self.vggModel = load_model('image_selection/weight_best.hdf') ###
         self.es = Elasticsearch("155.230.34.145:9200")
         print("weight_best loaded")
         self.g = tf.Graph()
@@ -19,11 +17,7 @@ class ImageSelectionModule(BaseModule):
             self.init_op = tf.group([tf.global_variables_initializer(), tf.tables_initializer()])
         self.g.finalize()
         print("graph defined")
-        
-        self.pattern = re.compile("[\|/|:|\*|?|\"|<|>|\|]")
-        
-    def process_data(self, query, image_save_path, download_limit = 50, silence=True):
-        query = self.pattern.sub(" ", query)
+    def process_data(self, query, image_save_path, download_limit = 50):
         es_query = {'query':{'match':{'keyword':query}}}
         res = self.es.search(index='image', body=es_query)
         if res['hits']['total']['value'] == 0:
@@ -33,8 +27,7 @@ class ImageSelectionModule(BaseModule):
         path_cached = ('./downloads')
         check_query_exist = os.listdir(path_cached)
         if query not in check_query_exist:  # case 1 : if query is new - download images & caption from google, scouter caching(url, caption), local caching(image path, caption)
-            if not silence:
-                print("query is new")
+            print("query is new")
             if type(query) is list:
                 topic = []
                 topic.append(query[0].replace('/','_'))
@@ -42,8 +35,7 @@ class ImageSelectionModule(BaseModule):
                 query = query.replace('/','_')
                 topic = []
                 topic.append(query)
-            if not silence:
-                print("query pre-processed")
+            print("query pre-processed")
             try_iter = 1
             while try_iter < 5: # handling VGG16 input error, no image found in google
                 try:
@@ -74,13 +66,16 @@ class ImageSelectionModule(BaseModule):
                 if e.errno != errno.EEXIST:
                     print("Failed to create directory!!!!!")
                     raise
-            localcache_name = 'localcache/%s.txt'%(query)
-            if try_iter != 5: # wychoi: 이미지 검색 안되면 caching 안함
-                with open(localcache_name, 'w') as f:
-                    for imgfile in image_list:
-                        f.write("%s\n"%imgfile)
-                    for captionfile in caption_list:
-                        f.write("%s\n"%captionfile)
+            localcache_img_name = 'localcache/%s_img.txt'%(query)
+            localcache_caption_name = 'localcache/%s_caption.txt'%(query)
+            with open(localcache_img_name, 'w') as f:
+                for imgfile in image_list:
+                    f.write("%s\n"%imgfile)
+            f.close()
+            with open(localcache_caption_name, 'w')as f2:
+                for captionfile in caption_list:
+                    f2.write("%s\n"%captionfile)
+            f2.close()
 
         else: # case 2 : if query is used before - use local cached data
             try:
@@ -92,15 +87,15 @@ class ImageSelectionModule(BaseModule):
                     query = query.replace('/','_')
                     topic = []
                     topic.append(query)
-                localcache_name = 'localcache/%s.txt'%(query)
-                file_list, image_list, caption_list = image_caption_get(localcache_name, download_limit)
+                localcache_img_name = 'localcache/%s_img.txt'%(query)
+                localcache_caption_name = 'localcache/%s_caption.txt'%(query)
+                file_list, image_list, caption_list = image_caption_get(localcache_img_name, localcache_caption_name)
             except IOError:
                 print("query does not have cached images and captions")
                 return False
 
-        nongraph_image_list, nongraph_caption_list = VGG_classifier(file_list, image_list, caption_list, self.vggModel, silence) ###
-        final_image = semantic_similarity_module(self.g, self.init_op, self.embedded_text, self.text_input,
-                                                 topic, nongraph_image_list, nongraph_caption_list, silence) ####
+        nongraph_image_list, nongraph_caption_list = VGG_classifier(file_list, image_list, caption_list, self.vggModel) ###
+        final_image = semantic_similarity_module(self.g, self.init_op, self.embedded_text, self.text_input, topic, nongraph_image_list, nongraph_caption_list) ####
         imgsave_path = image_save_path # path to save final recommended image
         temp_image = Image.open(final_image)
         temp_image = temp_image.convert('RGB')
